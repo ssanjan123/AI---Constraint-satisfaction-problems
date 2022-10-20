bec7b28d277cac678a582e3d0e9f15e336e31fe5
Wahid Sanjan <ssanjan@sfu.ca>
2022-10-20 05:52:18 -0700

    Created tournament function and parents for crossover

--- src/main.py
+++ src/main.py
@@ -4,11 +4,30 @@
 import random
 
 
+
+
+
+
+
+
+
+
+##Configurable Parameters/Global Variables
+populationSize = 100
+k = 4 ##How many times will the tournament run
 colors = (0,1,2,3,4)
+
 def getNodes():
     f = open("state_neighbours.txt")
     return len(f.readlines())
     
+class States:
+    def __init__(self,stateName,color,adjacentStates):
+        self.stateName = stateName
+        self.adjacentStates= adjacentStates
+        self.color = color
+    def getColor(self):
+        return self.color
 
 # Creates an adjacency matrix representing all the States
     #AB BC MB NB NL NS NT NU ON.......
@@ -41,29 +60,8 @@ def AdjacencyMatrix(AllStates):
         graph[i][i] = 0
     for v in graph:
         print(v)
+    return graph
     
-        ##print(obj.stateName, obj.color,obj.adjacentStates, sep =' ' )
-    
-##def GenerateInitPopulation(populationSize, chromosomeSize):
-
-
-# def create_chromosome():
-#         chromosome = []
-#         for i in range(n):
-#             chromosome.append(random.randint(0, 3)) #color ranging from 0 to 3 (4 colors)
-#         return chromosome
-
-class States:
-    def __init__(self,stateName,color,adjacentStates):
-        self.stateName = stateName
-        self.adjacentStates= adjacentStates
-        self.color = color
-    def getColor(self):
-        return self.color
-# class AdjacencyStates:
-#     def __init__(self, ) -> None:
-#         pass
-
 def StartState():
     f = open("state_neighbours.txt")
     ##print(f.read())
@@ -77,27 +75,67 @@ def StartState():
         AllStates.append(States(line.split(None, 1)[0],(random.randint(0, 3)),AdjacentList))
     return AllStates
 
-def createInitialPopulation(populationSize):
+
+############ Graph G(V,E)
+graph = AdjacencyMatrix(StartState()) #here graph is our
+######## adjacency matrix with vertices and edged having 0 or 1
+
+
+def createInitialPopulation():
     
     ##generation = 0
     population = []
     for i in range(populationSize):
         chromosome = StartState()
         population.append(chromosome)
+    return population
 
 
-def fitness(graph, chromosome): #here graph is our adjacency matrix with vertices and edged having 0 or 1
+def fitness(chromosome): 
     n = getNodes()
     edges = 0
     nonViolatingEdge = 0
     for i in range(n):
         for j in range(i, n):
-            if graph[i][j] == 1:
-                edges += 1
-                if(chromosome[i].color != chromosome[j].color):
-                    nonViolatingEdge += 1
+            try:
+                if graph[i][j] == 1:
+                    edges += 1
+                    if(chromosome[i].color != chromosome[j].color):
+                        nonViolatingEdge += 1
+            except TypeError as e:
+                print(j)
     fitness = nonViolatingEdge/edges
     return fitness
+
+def TournamentSelection(population):
+    parents = []
+    for j in range(k):
+        random.shuffle(population)
+        for i in range(0, populationSize-1, k):
+            if fitness(population[i]) > fitness(population[i+1]):
+                parents.append(population[i])
+            else:
+                parents.append(population[i+1])
+    return parents
+
+def crossover(parent1, parent2):
+    n = getNodes()
+    position = random.randint(2, n-2)
+    child1 = []
+    child2 = []
+    for i in range(position+1):
+        child1.append(parent1[i])
+        child2.append(parent2[i])
+    for i in range(position+1, n):
+        child1.append(parent2[i])
+        child2.append(parent1[i])
+    return child1, child2
+
+def produceNewGen(parents):
+    newChromosome = []
+    #for i in range(populationSize):
+
+
 
 def a():
     color = ["red","green","blue","yellow"]
@@ -122,7 +160,15 @@ def main():
     
     for obj in AllStates:
         print(obj.stateName, obj.color,obj.adjacentStates, sep =' ' )
-    AdjacencyMatrix(AllStates)
+    adj = AdjacencyMatrix(AllStates)
+
+    e = fitness(AllStates)
+    print(e)
+
+    newpop = TournamentSelection(createInitialPopulation())
+    for obj in newpop:
+        for i in obj:
+            print(i.stateName)
 
 main()
 #test1()
